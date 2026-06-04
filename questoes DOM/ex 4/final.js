b1ac294f9d8cc10a4258a16b196c4d2dc9c1c122
VICTOR VAILÕES SMANIOTTO <victor_smaniotto@estudante.sesisenai.org.br>
2026-06-03 21:51:37 -0300

    ex 5 feito

--- questoes DOM/ex 4/final.js
+++ questoes DOM/ex 4/final.js
@@ -1,1 +1,7 @@
-const nome = document.querySelector
+const nome = document.querySelector('#nome');
+const idade = document.querySelector('#idade');
+const curso = document.querySelector('#curso');
+
+const idadeNumero = Number(idade.value);
+
+console.log(`Nome: ${nome.value}\nIdade ${idadeNumero}\nTexto: ${curso.value}`);
